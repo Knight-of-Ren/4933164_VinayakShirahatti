05f407875cf9f75186e4b33a0a03c41ebaca7e2c
Vinayak <vinayak23052003@gmail.com>
2025-08-04 20:26:49 +0530

    completed 2 hacker earth problems

---
+++ Hacker_earth_programs/plusMinuS.c
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void plusMinus(vector<int> arr) {
+    int n = arr.size();
+    double positive_count = 0;
+    double negative_count = 0;
+    double zero_count = 0;
+
+    // Iterate through the array and count positive, negative, and zero elements
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            positive_count++;
+        } else if (arr[i] < 0) {
+            negative_count++;
+        } else {
+            zero_count++;
+        }
+    }
+
+    // Set precision for floating-point output to 6 decimal places
+    cout << fixed << setprecision(6);
+
+    // Print the ratios
+    cout << positive_count / n << endl;
+    cout << negative_count / n << endl;
+    cout << zero_count / n << endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
