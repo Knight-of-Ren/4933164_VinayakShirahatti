962c51d4511f4307707fdc6eb600c1df95294577
Vinayak Shirahatti <vinayak23052003@gmail.com>
2025-08-14 23:32:41 +0530

    new problems solved except XOR2 since it is bugged

---
+++ HackerRank/Week1/Pangrams.c
@@ -0,0 +1,135 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* pangrams(char* s) {
+    // A boolean array to act as a checklist for letters 'a' through 'z'
+    bool alphabet_seen[26] = {false};
+
+    // Iterate through every character in the input string
+    for (int i = 0; i < strlen(s); i++) {
+        char current_char = s[i];
+        
+        // We only care about alphabetic characters
+        if (isalpha(current_char)) {
+            // Convert the character to lowercase to make the check case-insensitive
+            char lower_char = tolower(current_char);
+            
+            // Calculate the index (0-25) and mark this letter as 'seen'
+            alphabet_seen[lower_char - 'a'] = true;
+        }
+    }
+
+    // Check our checklist to see if any letter is missing
+    for (int i = 0; i < 26; i++) {
+        if (!alphabet_seen[i]) {
+            // If we find any letter that wasn't seen, it's not a pangram
+            return "not pangram";
+        }
+    }
+    
+    // If the loop completes, all 26 letters were found
+    return "pangram";
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = pangrams(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
